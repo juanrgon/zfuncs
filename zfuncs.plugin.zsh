92147989635eccfeef92a8c77d1b07bae21b9111
Juan Gonzalez <jrg2156@gmail.com>
2018-07-04 13:33:49 -0400

    Bug fixes

--- zfuncs.plugin.zsh
+++ zfuncs.plugin.zsh
@@ -1,14 +1,15 @@
 if ! (( ${+ZFUNCS_PATH} )); then
-    ZFUNCS_PATH=$HOME/zfuncs
+    export ZFUNCS_PATH=$HOME/zfuncs
 fi
 
 if [[ ! -d $ZFUNCS_PATH ]]; then
     mkdir -p $ZFUNCS_PATH
 fi
 
+export fpath=($ZFUNCS_PATH $fpath)
 for file in $(find $ZFUNCS_PATH ! -name '_*' -type f)
 do
-    autoload -U $ZFUNCS_PATH/$file
+    autoload -U $file
 done
 
 autoload -U $(dirname $0:a)/zfuncs
